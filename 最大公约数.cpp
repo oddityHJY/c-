1db3d001b856f0843dc1fa4cb8c1c9e5054abd37
oddityHJY <48441181+oddityHJY@users.noreply.github.com>
2019-04-21 15:26:11 +0800

    最大公约数.cpp

---
+++ 最大公约数.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+class Number{
+public:
+    Number(int n1);
+	int maxDivisor(Number n2);
+	void show();
+private:                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+	int n;
+};
+int main() {
+    int n, n1, n2;
+    std::cin >> n;
+    for(int i = 0; i < n; ++i) {
+        std::cin >> n1 >> n2;
+        Number no1(n1), no2(n2);
+        Number no3 = no1.maxDivisor(no2);  //最大公约数
+        no3.show();
+    }
+}
+Number::Number(int n1) {
+	n = n1;
+};
+int Number::maxDivisor(Number n2) {
+	int a=n, b=n2.n, c=0;
+	while(a != b) {
+		if (a < b){
+			c = a;
+			a = b;
+			b = c;
+		}
+			c = b;
+			b = a - b;
+			a = c;
+	}
+	c = a;
+	return c;
+}
+void Number::show() {
+	std::cout << n << endl;
+}
